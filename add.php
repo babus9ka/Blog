1ea8319c4b6b1b59c3759c8e1ab70b0b9c2ed73b
babus9ka <babus9ka2017@yandex,ru>
2022-04-19 18:10:07 +0300

    database connection, layout

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+include "header.php";
+?>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add Post</div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea cols="20" name="description" id="editor" class="form-control"></textarea>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    ClassicEditor
+        .create( document.querySelector( '#editor' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+</script>
+<style type="text/css">
+    .card{
+        margin-top: 10px;
+    }
+</style>
